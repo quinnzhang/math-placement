767952ab08d95e2146031d6899a47ed46850b2a8
Quinn Zhang <quinn.zhang@yale.edu>
2013-06-11 11:18:41 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MathPlacement::Application.config.secret_key_base = '84378a3df269ed9459ddda7fdb6db640ae0c33dde3d5f6d4d0625729a8d0e1a55b5a4a117b9ad7e772e8c1592c97e74e252b87d60545f74df892f34f47d733f5'
