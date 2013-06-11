65da447e01d41dac478069010702dbbcf8dd2155
Quinn Zhang <quinn.zhang@yale.edu>
2013-06-11 11:43:22 -0400

    updated and seeded Student db

---
+++ db/migrate/20130611154237_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :net_id
+      t.integer :upi
+      t.string :college
+      t.string :preliminary_placement
+      t.string :final_placement
+
+      t.timestamps
+    end
+  end
+end
